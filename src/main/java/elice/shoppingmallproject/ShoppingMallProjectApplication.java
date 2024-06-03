9bbd95c7d6b17ab89292952402c0e44eb0a81cdb
opixxx <opix0306@naver.com>
2024-06-03 21:16:16 +0900

    initial commit

---
+++ src/main/java/elice/shoppingmallproject/ShoppingMallProjectApplication.java
@@ -0,0 +1,13 @@
+package elice.shoppingmallproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMallProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/elice/shoppingmallproject/ShoppingMallProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package elice.shoppingmallproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

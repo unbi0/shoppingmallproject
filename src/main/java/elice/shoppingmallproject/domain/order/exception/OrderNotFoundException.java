ca5b992e230303632c6814f5317a44b7b6661e8e
[Cloud3] 정재형 <opix0306@naver.com>
2024-06-03 14:27:54 +0000

    Revert "Merge branch 'dev' into 'master'"
    This reverts merge request !9

--- src/main/java/elice/shoppingmallproject/domain/order/exception/OrderNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package elice.shoppingmallproject.domain.order.exception;
-
-public class OrderNotFoundException extends RuntimeException{
-
-    public OrderNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/elice/shoppingmallproject/domain/order/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package elice.shoppingmallproject.domain.order.exception;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}

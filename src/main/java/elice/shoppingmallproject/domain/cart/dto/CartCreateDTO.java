f21f9ba672be0b0a9bb3ec3cb78dd8a0c2ecde31
유동원 <dongwon@yudong-won-ui-MacBookAir.local>
2024-06-07 13:01:31 +0900

    코드리뷰 반영

--- src/main/java/elice/shoppingmallproject/domain/cart/dto/CartCreateDTO.java
+++ src/main/java/elice/shoppingmallproject/domain/cart/dto/CartCreateDTO.java
@@ -1,35 +1,12 @@
 package elice.shoppingmallproject.domain.cart.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CartCreateDTO {
     private Long optionId;
     private Long userId;
     private int quantity;
-
-    // Getters and Setters
-
-    public Long getOptionId() {
-        return optionId;
-    }
-
-    public void setOptionId(Long optionId) {
-        this.optionId = optionId;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
 }
-
-

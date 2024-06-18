e57bd2cc56823b66673bf3d270502308cd175b68
opixxx <opix0306@naver.com>
2024-06-18 17:28:55 +0900

    feat: Category 조회 오름차순

--- src/main/java/elice/shoppingmallproject/domain/category/repository/CategoryRepository.java
+++ src/main/java/elice/shoppingmallproject/domain/category/repository/CategoryRepository.java
@@ -1,13 +1,19 @@
 package elice.shoppingmallproject.domain.category.repository;
 
 import elice.shoppingmallproject.domain.category.entity.Category;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Boolean existsByName(String name);
+
+    @Query("SELECT c FROM Category c ORDER BY c.createAt ASC")
+    List<Category> findAllByCreateAt();
+
 
 }
 

fd644aede70ea9ae7817f5e00d970d2b108cb511
[Cloud3] 성대현 <gneel2@naver.com>
2024-06-18 15:14:10 +0900

    124124

--- src/main/java/elice/shoppingmallproject/global/config/S3Config.java
+++ src/main/java/elice/shoppingmallproject/global/config/S3Config.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.PropertySource;
 
 
 @Configuration
-@PropertySource("classpath:application-s3.properties")
+@PropertySource("classpath:application-private.properties")
 public class S3Config {
 
     @Value("${cloud.aws.credentials.accessKey}")
